0ca6b03528f2a9f3ded9f9a29586e3dc645530bc
Ankur Auti <ankurauti@gmail.com>
2024-12-26 01:30:05 +0530

    v0.65 chnages done

--- src/components/buttons/google-signup-button.jsx
+++ src/components/buttons/google-signup-button.jsx
@@ -9,35 +9,27 @@ const GoogleSignUpButton = ({
   text,
   className = "",
 }) => {
-  const handleClick = (event) => {
-    // Find and trigger the Google Login button
-    const googleLoginButton = event.currentTarget.querySelector(
-      ".google-login-trigger"
-    );
-    if (googleLoginButton) {
-      googleLoginButton.querySelector('div[role="button"]')?.click();
-    }
-  };
-
   return (
     <div className={`relative w-full ${className}`}>
-      <button
+      <Button
         variant="ghost"
-        className="flex items-center py-2.5 rounded-md w-full bg-[#F3F6F8] justify-center border border-[#E7E7EA]  font-medium text-[0.875rem] shadow-[0px_6px_16px_rgba(0,0,0,0.04)]"
-        onClick={handleClick}
+        className="w-full bg-[#F3F6F8] justify-center border border-[#E7E7EA] font-medium text-[0.875rem] shadow-[0px_6px_16px_rgba(0,0,0,0.04)] py-[20px]"
         disabled={disabled}
+        style={{ pointerEvents: "none" }}
       >
         <img src="/images/google.svg" alt="google img" className="h-5 mr-2" />
-
         {text}
-      </button>
-      {/* Hidden Google Login component */}
-      <div className="google-login-trigger absolute top-0 left-0 w-full h-full opacity-0">
+      </Button>
+      <div className="absolute inset-0 opacity-0">
         <GoogleLogin
           onSuccess={onSuccess}
           onError={onError}
           useOneTap
           disabled={disabled}
+          type="standard"
+          theme="outline"
+          size="large"
+          width="100%"
         />
       </div>
     </div>
