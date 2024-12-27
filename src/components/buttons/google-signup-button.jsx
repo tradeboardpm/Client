385f57293f1a32bf8c19550d1367b05bb70825ae
Ankur Auti <ankurauti@gmail.com>
2024-12-27 21:03:59 +0530

    v0.66 with color changes and logo added

--- src/components/buttons/google-signup-button.jsx
+++ src/components/buttons/google-signup-button.jsx
@@ -9,19 +9,7 @@ const GoogleSignUpButton = ({
   className = "",
 }) => {
   return (
-    <div className="relative w-full">
-      {/* Visible button that matches other buttons */}
-      <button
-        type="button"
-        disabled={disabled}
-        className="w-full bg-[#F3F6F8] dark:bg-[#434445] justify-center border dark:border-[#303031] border-[#E7E7EA] font-medium text-[0.875rem] shadow-[0px_6px_16px_rgba(0,0,0,0.04)] py-2.5 flex items-center rounded-md"
-      >
-        <img src="/images/google.svg" alt="google img" className="h-5 mr-2" />
-        {text}
-      </button>
 
-      {/* Invisible Google Login button that covers entire area */}
-      <div className="absolute inset-0 w-[436px] h-full opacity-100">
         <GoogleLogin
           onSuccess={onSuccess}
           onError={onError}
@@ -36,8 +24,6 @@ const GoogleSignUpButton = ({
             height: "100%",
           }}
         />
-      </div>
-    </div>
   );
 };
 
