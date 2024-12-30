f69d8e85942dedb212e51957d9d021eb0e063030
Ankur Auti <ankurauti@gmail.com>
2024-12-30 16:36:39 +0530

    added complete trade dialog

--- src/utils/getVersion.js
+++ src/utils/getVersion.js
@@ -1,21 +1,9 @@
-// src/utils/getVersion.js
 import { execSync } from "child_process";
 
 export function getGitVersion() {
   try {
-    // First check if there are any tags
-    const hasTag = execSync("git tag").toString().trim();
-
-    if (!hasTag) {
-      return "0.0.1"; // Return initial version if no tags exist
-    }
-
     // Get the latest tag using git describe
-    const tag = execSync(
-      "git describe --tags $(git rev-list --tags --max-count=1)"
-    )
-      .toString()
-      .trim();
+    const tag = execSync("git describe --tags").toString().trim();
 
     // Remove 'v' prefix if it exists
     return tag.startsWith("v") ? tag.slice(1) : tag;
